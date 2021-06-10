0133af98e9221d80a5f32f367beb4b7db6028785
mlpalomino74 <ml.palomino74@uniandes.edu.co>
2021-06-09 21:53:01 -0500

    comentarios 2

---
+++ Task-3.R
@@ -0,0 +1,18 @@
+#Elaborado por: Monica Palomino 200611835, Paula Urbina 201633091, Nicolás Danies 201822710
+#Colaborador: Todos los miembros!
+#Fecha de elaboración:09 de junio del 2021
+#Fecha última modificación: 10 de junio del 2021
+
+#Se rralizó el Taller B
+
+#initial configuration
+
+library(tidyr)
+library(data.table)
+
+
+#Punto 1.1 Importar datos espaciales
+
+#Punto 1.1.1 Importar shapefiles VIAS
+
+#Punto 1.1.2
